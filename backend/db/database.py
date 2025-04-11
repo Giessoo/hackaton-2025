8cd62f19afc9a8ae7c6117ad09495a610e5d43ee
Fizruk02 <fizrukvush@mail.com>
2025-04-11 17:22:19 +0300

    +

--- backend/db/database.py
+++ backend/db/database.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "postgresql://postgres:1234567890@localhost:5432/Hackaton_secon_2025"
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
