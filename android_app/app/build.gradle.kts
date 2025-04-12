44c0a2ada2e35f3d91c0aa34f5ac2df7f8cfab58
blust <blust.0.official@gmail.com>
2025-04-12 21:50:21 +0300

    add dependency

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -33,13 +33,15 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.retrofit)
+    // implementation(libs.retrofit_converter)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
